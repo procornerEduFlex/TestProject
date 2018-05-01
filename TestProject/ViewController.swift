1c75eb8256cc85ab8916242faa8efeeba3abb746
Procorner Eduflex <pcepl@virajs-macbook-pro.local>
2018-05-01 17:49:52 +0530

    added some lines for print

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let test = "test"
+        print(test)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
