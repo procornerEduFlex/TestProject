1aebc81219982ce4a6117e6ea124cafa1bb87424
Procorner Eduflex <pcepl@virajs-macbook-pro.local>
2018-05-01 17:39:50 +0530

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Procorner Eduflex on 01/05/18.
+//  Copyright © 2018 MyCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
