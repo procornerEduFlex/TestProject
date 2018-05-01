8760efa72fe7589624ef653f34b303c2dcf886dc
Procorner Eduflex <pcepl@virajs-macbook-pro.local>
2018-05-01 17:51:24 +0530

    delete line

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let test = "test"
-        print(test)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
